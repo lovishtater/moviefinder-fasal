be06e5e76b38941aa4ac08d7c9b8c363b6e9b8fe
Lovish Tater <lovishtater@gmail.com>
2022-06-24 15:43:03 +0530

    Project setup

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/css/index.css';
+import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider breakpoint={["xs", "sm", "md", "lg", "xl"]}>
+    <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
